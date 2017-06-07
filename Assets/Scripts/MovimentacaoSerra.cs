068ae7bd8839a0ede2fe4726476a0ae51b94d961
Samuel Queiroz Silva <samqueiroz90@gmail.com>
2017-06-06 23:26:53 -0400

    addding

---
+++ Assets/Scripts/MovimentacaoSerra.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentacaoSerra : MonoBehaviour {
+
+	private Rigidbody2D rb2D;
+
+
+	// Use this for initialization
+	void Start () {
+
+		rb2D = GetComponent<Rigidbody2D> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ( /*rb2D.velocity.x >= 0 && */  rb2D.position.x > 4) {
+			Debug.Log ("Direita: " + rb2D.position.x);
+			rb2D.velocity = new Vector2 (rb2D.velocity.x + 0.05f , rb2D.velocity.y);
+			
+		} else if (/* rb2D.velocity.x < 0 && */ rb2D.position.x < -4) {
+			Debug.Log ("Esquerda: " + rb2D.position.x);
+			rb2D.velocity = new Vector2 (rb2D.velocity.x - 0.05f , rb2D.velocity.y);
+			
+		}
+
+	}
+}
