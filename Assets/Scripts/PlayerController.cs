4e197e5b44dcd36e4cf98a285ced9581ba36fea0
xsery <fcmaug@gmail.com>
2017-06-08 10:03:24 -0400

    Merge branch 'master' into stage01
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb2D;
+
+	[SerializeField]
+	private float velocidade = 0;
+
+	float horizontal;
+
+	// Use this for initialization
+	void Start () {
+
+		rb2D = GetComponent<Rigidbody2D> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		horizontal = Input.GetAxis ("Horizontal");
+
+		Movimentar (horizontal);
+		
+	}
+
+	private void Movimentar(float h){
+
+		rb2D.velocity = new Vector2 (h * velocidade, rb2D.velocity.y);
+	}
+}
