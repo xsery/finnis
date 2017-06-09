f966addf6df38d673ad9c2baa1490a781c2b7474
Samuel Queiroz Silva <samqueiroz90@gmail.com>
2017-06-08 23:40:28 -0400

    Add stage 4
    Add stage 4

--- Assets/Scripts/MovimentacaoSerra.cs
+++ Assets/Scripts/MovimentacaoSerra.cs
@@ -11,21 +11,31 @@ public class MovimentacaoSerra : MonoBehaviour {
 	void Start () {
 
 		rb2D = GetComponent<Rigidbody2D> ();
+		rb2D.velocity = new Vector2 (1.5f, rb2D.velocity.y);
+
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
-		if ( /*rb2D.velocity.x >= 0 && */  rb2D.position.x > 4) {
-			Debug.Log ("Direita: " + rb2D.position.x);
-			rb2D.velocity = new Vector2 (rb2D.velocity.x + 0.05f , rb2D.velocity.y);
+		//rb2D.rotation = new Vector2 (rb2D.rotation + 1f, rb2D.rotation.y);
+
+		rb2D.transform.Rotate (0,0,10);
+
+		if (rb2D.position.x > 4) {
+			//Debug.Log ("Posicao 1: " + rb2D.position.x);
+			//Debug.Log ("Velocidade 1: " + rb2D.velocity.y);
+			rb2D.velocity = new Vector2 (-1.5f , rb2D.velocity.y);
 			
-		} else if (/* rb2D.velocity.x < 0 && */ rb2D.position.x < -4) {
-			Debug.Log ("Esquerda: " + rb2D.position.x);
-			rb2D.velocity = new Vector2 (rb2D.velocity.x - 0.05f , rb2D.velocity.y);
+		}if (rb2D.position.x < -4) {
+			//Debug.Log ("Posicao 2: " + rb2D.position.x);
+			//Debug.Log ("Velocidade 2: " + rb2D.velocity.y);
+			rb2D.velocity = new Vector2 (1.5f, rb2D.velocity.y);
 			
 		}
+
+		//Debug.Log (rb2D.velocity.x );
 
 	}
 }
