8eca6b9548733dc018cbcfb0442691cdc228a047
xsery <fcmaug@gmail.com>
2017-06-12 22:40:59 -0400

    Montagem de Cenários [Chão, Parede, Teto e Objetos] (13)

---
+++ Assets/Scripts/FinnisMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinnisMovement : MonoBehaviour {
+
+
+	public float velocity;
+	public float jumpForce;
+
+	private Rigidbody2D rb2d;
+	private bool facingRight = true;
+	private bool jump;
+	private Animator anim;
+	private bool grounded = false;
+
+	private Transform groundCheck;
+
+
+	// Use this for initialization
+	void Start () {
+
+		rb2d = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+		groundCheck = gameObject.transform.Find ("GroundCheck");
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
+
+		if (Input.GetButtonDown ("Jump") && grounded) {
+
+			jump = true;
+		}
+	}
+
+	void FixedUpdate(){
+
+		float axis = Input.GetAxisRaw ("Horizontal");
+
+		rb2d.velocity = new Vector2 (axis * velocity, rb2d.velocity.y);
+
+		if (axis > 0 && !facingRight) {
+			Flip ();
+		}
+		if (axis < 0 && !facingRight) {
+			Flip ();
+		}
+
+		if (jump) {
+			rb2d.AddForce (new Vector2 (0, jumpForce));
+			jump = false;
+		}
+	}
+
+	void Flip(){
+
+		facingRight = !facingRight;
+
+		Vector3 finnisScale = transform.localScale;
+		finnisScale.x *= -1;
+		transform.localScale = finnisScale;
+	}
+	
+}
